9a2984a9ec2dceafa5449f15e2ac73ca1a17a971
grindhold <grindhold@gmx.net>
2014-09-07 05:06:49 +0200

    continued working on invitationsystem

--- backend/invitation.go
+++ backend/invitation.go
@@ -4,11 +4,68 @@ import (
 	"github.com/emicklei/go-restful"
 	"labix.org/v2/mgo/bson"
     "net/http"
+  "errors"
+  "math/rand"
+  "encoding/hex"
+  "encoding/binary"
+  "crypto/sha512"
 )
 
+type InvitationWrapper struct {
+  Invitation *Invitation
+}
+
 type Invitation struct {
-	Id    bson.ObjectId `bson:"_id,omitempty" json:"id,omitempty"`
+  MongoId  bson.ObjectId `bson:"_id,omitempty"`
+	Id    string `json:"id,omitempty"`
 	Email string
+  Username string
+  Password string
+  Organization string
+}
+
+func LoadInvitationByEmail(eml string) (*Invitation, error) {
+    x := new(Invitation)
+    err := mongo.DB("void").C("invitations").Find(bson.M{"email":eml}).One(&x)
+    return x,err
+}
+
+func LoadInvitationById(id string) (*Invitation, error){
+    x := new(Invitation)
+    err := mongo.DB("void").C("invitations").Find(bson.M{"id":id}).One(&x)
+    return x,err
+}
+
+func (i *Invitation) generateId() string {
+    s := make([]byte, 8)
+    binary.LittleEndian.PutUint64(s,uint64(rand.Int63()))
+    s = append(s, []byte(i.MongoId.Hex())...)
+    s = append(s, []byte(i.Email)...)
+    sum := sha512.Sum512(s)
+    return hex.EncodeToString(sum[0:64])
+}
+
+func (i *Invitation) Invite () error {
+    if _, err := LoadInvitationByEmail(i.Email) ; err == nil {
+        return errors.New("invitation to this email already exists")
+    }
+    i.MongoId = bson.NewObjectId()
+    i.Id = i.generateId()
+    err := mongo.DB("void").C("invitations").Insert(i)
+    return err
+}
+
+func (i *Invitation) Activate (d *Invitation) error {
+    user := new(User)
+    user.Name = d.Username
+    user.Email = i.Email
+    user.Organization = d.Organization
+    user.SetPassword(d.Password)
+    if err := user.Save() ; err == nil {
+        return mongo.DB("void").C("invitations").RemoveId(i.Id)
+    } else {
+        return err
+    }
 }
 
 type InvitationResource struct{}
@@ -21,11 +78,19 @@ func (r InvitationResource) Register(wsContainer *restful.Container) {
 
 	ws.Route(ws.GET("/{entry}").Filter(authFilter).To(r.getInvitation))
 	ws.Route(ws.POST("/").Filter(authFilter).To(r.createInvitation))
+  ws.Route(ws.PUT("/{entry}").Filter(authFilter).To(r.editInvitation))
+  ws.Route(ws.DELETE("/{entry}").Filter(authFilter).To(r.deleteInvitation))
 	wsContainer.Add(ws)
 }
 
 func (r InvitationResource) getInvitation(req *restful.Request, resp *restful.Response) {
-	//TODO:implement
+  if inv, err := LoadInvitationById(req.PathParameter("entry")); err == nil {
+      iw := new(InvitationWrapper)
+      iw.Invitation = inv
+      resp.WriteEntity(iw)
+  } else {
+      resp.WriteErrorString(http.StatusNotFound, "no such invitation")
+  }
 }
 
 func (r InvitationResource) createInvitation(req *restful.Request, resp *restful.Response) {
@@ -34,5 +99,33 @@ func (r InvitationResource) createInvitation(req *restful.Request, resp *restful
         resp.WriteErrorString(http.StatusForbidden, "you must be logged in to do that")
         return
     }
-	//TODO:implement
+    iw := new(InvitationWrapper)
+    if err := req.ReadEntity(iw) ; err == nil {
+        if err := iw.Invitation.Invite() ; err != nil {
+            resp.WriteErrorString(http.StatusInternalServerError, err.Error())
+        } else {
+            resp.WriteEntity(&iw)
+        }
+    }else{
+        resp.WriteErrorString(http.StatusBadRequest, "bad invitation")
+    }
+}
+
+func (r InvitationResource) editInvitation (req *restful.Request, resp *restful.Response) {
+  if inv, err := LoadInvitationById(req.PathParameter("entry")); err == nil {
+      iw := new(InvitationWrapper)
+      if err := req.ReadEntity(iw) ; err == nil {
+          if err := inv.Activate(iw.Invitation) ; err != nil{
+              resp.WriteErrorString(http.StatusBadRequest, err.Error())
+          }
+      } else {
+        resp.WriteErrorString(http.StatusBadRequest, "bad invitation")
+      }
+  } else {
+      resp.WriteErrorString(http.StatusNotFound, "no such invitation")
+  }
+}
+
+func (r InvitationResource) deleteInvitation (req *restful.Request, resp *restful.Response) {
+    resp.WriteEntity(Empty{})
 }
--- frontend/ember/invitation.em
+++ frontend/ember/invitation.em
@@ -1,16 +1,33 @@
 class App.InvitationsNewRoute extends Ember.Route
     model : -> Ember.Object.create
                    email: ""
+                   link: ""
+
+class App.InvitationRoute extends Ember.Route
+    model : (params) -> @store.find 'invitation', params.ident
 
 class App.InvitationSerializer extends DS.RESTSerializer with CapitalAttrs
     keyForRelation: (key,name) -> key
 
 class App.Invitation extends DS.Model
     email : DS.attr 'string'
+    name : DS.attr 'string'
+    organization : DS.attr 'string'
+    password : DS.attr 'string'
 
 class App.InvitationsNewController extends Ember.Controller
+    inviteLink : ~> "#{HOST}/#/invitation/#{@content.link}"
+    linkPresent : ~> @content.link != ""
     actions:
         invite : ->
             i = @store.createRecord 'invitation',
-                  email: @content.email
-            i.save()
+                  email: @model.email
+            self = this
+            i.save().then (x) ->
+                self.content.link = x.id
+
+class App.InvitationController extends Ember.ObjectController
+    actions :
+        redeem : ->
+            @content.save()
+            @transitionTo "index"
